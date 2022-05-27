e211b12295aa1cbb5da58f6816364d78c20dc4e7
Van <84937597+vandalit@users.noreply.github.com>
2022-05-26 20:26:56 -0400

    diferenciando variables, locales, metodos

---
+++ 00 Ivan Espinoza (ayudante)/Ruby/terrible.rb
@@ -0,0 +1,8 @@
+puts "ingrese un animal"
+animal = gets.chomp
+
+def animalMet(animalEntrada)
+    puts "aquí tengo un #{animalEntrada}"
+end
+
+animalMet(animal)
--- 00 Ivan Espinoza (ayudante)/Ruby/votar.rb
+++ 00 Ivan Espinoza (ayudante)/Ruby/votar.rb
@@ -4,7 +4,7 @@ puts "ingrese su edad"
 edad = gets.chomp.to_i
 
 if edad >= 18
-puts "puedes votar"
+    puts "puedes votar"
 else
-puts "no puedes votar"
+    puts "no puedes votar"
 end
