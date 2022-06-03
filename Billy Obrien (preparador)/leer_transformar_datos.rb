f720f1783d43d245af997495b8938272d39bbd83
Van <84937597+vandalit@users.noreply.github.com>
2022-06-03 01:38:50 -0400

    algo es algo

--- Billy Obrien (preparador)/leer_transformar_datos.rb
+++ Billy Obrien (preparador)/leer_transformar_datos.rb
@@ -1,4 +1,5 @@
-data = open('data').read.chomp.split(",")
+#data = open('data').read.chomp.split(",")
+data = open('data').read.chomp.split("\n") #ya, si usamos backlash, obtenemos lo opuesto, "cortamos" con cada salto de linea. Ahora solo queda estudiar como le damos ambos argumentos
 nuevos_datos = []
 data.each do |elemento|
     if elemento.to_i > 20
